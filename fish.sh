0252625cf90bf052c55c9a799462c38a306dc27d
Dima-git-hak <82115394+Dima-git-hak@users.noreply.github.com>
2021-04-23 20:17:33 +0500

    Add files via upload

--- fish.sh
+++ fish.sh
@@ -29,6 +29,6 @@ echo -e '\nType social engineering words:(like free-money, best-pubg-tricks)'
 echo -e "\e[31mDon't use space just use '-' between social engineering words\e[0m"
 echo -en "\e[32m=>\e[0m "
 read words
-echo -e "\nGenerating dima ссылка...\n"
+echo -e "\nGenerating fish ссылка...\n"
 final=$mask-$words@$shorter
-echo -e "Here is the dima URL:\e[32m ${final} \e[0m\n"
+echo -e "Here is the fish URL:\e[32m ${final} \e[0m\n"
